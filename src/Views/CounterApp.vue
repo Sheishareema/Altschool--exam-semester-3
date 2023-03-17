5a7c35261254a8083daf2a66fa0ca7589ac85e3b
Ameerah <sheisameerah@gmail.com>
2023-03-17 08:31:48 +0000

    first commit

---
+++ src/Views/CounterApp.vue
@@ -0,0 +1,157 @@
+<template>
+  <NavBar />
+  <div class="wrap">
+    <div class="count">
+      <div class="ui">
+        <button @click="increment" class="add">+</button>
+        <div class="interface">{{ count }}</div>
+        <button @click="decrement" class="minus">-</button>
+      </div>
+      <div class="btn">
+        <input
+          type="number"
+          v-model="value"
+          placeholder="Enter a number"
+          class="input"
+        />
+        <button @click="setValue(value)" class="set">Set Value</button>
+        <button @click="reset" class="reset">Reset</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import useCounter from "@/components/Counter.js";
+import NavBar from "./NavBar.vue";
+
+export default {
+  setup() {
+    const { count, increment, decrement, reset, setValue } = useCounter();
+
+    let value = "";
+
+    return {
+      count,
+      increment,
+      decrement,
+      reset,
+      value,
+      setValue,
+    };
+  },
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style scoped>
+.count {
+  padding: 0.2% 7%;
+  background-color: grey;
+  color: #fff;
+  color: black;
+  width: max-content;
+  height: auto;
+  border-radius: 10px;
+  box-shadow: 2xl red;
+  margin-inline: auto;
+  padding: 2%;
+}
+
+.ui {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+  margin-bottom: 30px;
+}
+
+.btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+}
+
+.add {
+  background-color: green;
+  color: white;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 5px;
+  font-size: 1.5rem;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.add:hover {
+  opacity: 0.8;
+}
+
+.minus {
+  background-color: #fd3668;
+  color: white;
+  padding: 5px 10px;
+  border: none;
+  border-radius: 5px;
+  font-size: 1.5rem;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.minus:hover {
+  background-color: #ff0846;
+}
+
+.interface {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ffffff;
+  padding: 10px 25px;
+  width: 50px;
+  height: 50px;
+  border: none;
+  border-radius: 5px;
+  font-size: 1.5rem;
+  color: black;
+  transition: 0.5s;
+}
+.reset {
+  background-color: #fd3668;
+  color: white;
+  padding: 10px 25px;
+  border: none;
+  border-radius: 5px;
+  font-size: 1.5rem;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.reset:hover {
+  background-color: #ff0846;
+}
+
+.input {
+  background-color: #ffffff;
+  padding: 10px 25px;
+  border: none;
+  border-radius: 5px;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.set {
+  background-color: green;
+  color: white;
+  padding: 10px 25px;
+  border: none;
+  border-radius: 5px;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: 0.5s;
+}
+</style>
---
+++ src/Views/HomeApp.vue
@@ -0,0 +1,74 @@
+<template>
+  <NavBar />
+  <div class="home">
+    <div class="txt">
+      <p>
+        Counter App - Altschool Africa School of Engineering, Frontend Stack
+        Third Semester Exam
+      </p>
+      <router-link to="/counter">Go to Counter</router-link>
+    </div>
+  </div>
+  <FooterApp />
+</template>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: center;
+  align-items: center;
+  margin: 7% 9%;
+}
+.img {
+  max-width: 700px;
+  margin-left: 3%;
+}
+
+.img img {
+  width: 100%;
+  height: 30%;
+  opacity: 0.3;
+  border-radius: 10px;
+}
+.txt {
+  display: flex;
+  flex-direction: column;
+
+  max-width: 400px;
+}
+
+.txt h1 {
+  background-image: -webkit-linear-gradient(45deg, #fa84a2, #64bffc);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+span {
+  color: #fa84a2;
+}
+.txt p {
+  margin-bottom: 26px;
+  font-size: 1.5rem;
+  color: #c5c5c5;
+}
+.txt a {
+  text-decoration: none;
+  padding: 10px 25px;
+  background-color: blue;
+  color: #fff;
+  transition: 0.5s;
+}
+.txt a:hover {
+  background-color: #fa84a2;
+}
+</style>
+
+<script>
+import NavBar from "./NavBar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
